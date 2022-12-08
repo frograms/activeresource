02902f75aa2587362435207142eb574e88867662
buzz <buzz@watcha.com>
2022-12-08 15:06:30 +0900

    move resource json

--- lib/active_resource/server_side.rb
+++ lib/active_resource/server_side.rb
@@ -6,4 +6,4 @@ end
 
 require 'active_resource/exceptions'
 require 'active_resource/api_type_name_object_map'
-require 'active_resource/server_side/resource_json'
+require 'active_resource/resource_json'
