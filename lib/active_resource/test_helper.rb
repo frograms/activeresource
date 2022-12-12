2993952c6de60ee784ff35c87e8acacc653813de
buzz <buzz@watcha.com>
2022-12-12 13:28:23 +0900

    do not use notification

--- lib/active_resource/test_helper.rb
+++ lib/active_resource/test_helper.rb
@@ -1,64 +1,54 @@
 module ActiveResource
   module TestHelper
     class CaptureConnection < Connection
-      cattr_accessor :capture_id
-      class << self
-        attr_writer :mock_body
-
-        def mock_body
-          @mock_body ||= '{}'
-        end
-      end
 
       def request(method, path, *arguments)
-        if capture_id
-          ActiveSupport::Notifications.instrument("capture.active_resource.#{capture_id}") do |capture_payload|
-            ActiveSupport::Notifications.instrument("request.active_resource") do |payload|
-              payload[:method]      = method
-              payload[:request_uri] = "#{site.scheme}://#{site.host}:#{site.port}#{path}"
-              payload[:connection]  = http
-              payload[:method] = method
-              payload[:path] = path
-              case method
-              when :get, :delete, :head
-                payload[:headers] = arguments[0]
-              else
-                payload[:body_s] = arguments[0]
-                payload[:body] = format.decode_as_it_is(arguments[0]) if arguments[0].present?
-                payload[:headers] = arguments[1]
-              end
-              capture_payload.update(payload)
-            end
+        result = ActiveSupport::Notifications.instrument("request.active_resource") do |payload|
+          payload[:method]      = method
+          payload[:request_uri] = "#{site.scheme}://#{site.host}:#{site.port}#{path}"
+          payload[:connection]  = http
+          payload[:method] = method
+          payload[:path] = path
+          case method
+          when :get, :delete, :head
+            payload[:headers] = arguments[0]
+          else
+            payload[:body_s] = arguments[0]
+            payload[:body] = format.decode_as_it_is(arguments[0]) if arguments[0].present?
+            payload[:headers] = arguments[1]
           end
-          result = OpenStruct.new
-          result.body = self.class.mock_body
-          result
-        else
-          super
-        end
-      end
-    end
-
-    module Methods
-      def resource_request_controller(id: SecureRandom.base64(10))
-        CaptureConnection.capture_id = id
-        ActiveSupport::Notifications.subscribe("capture.active_resource.#{id}") do |name, start_time, end_time, _, payload|
           uri = URI.parse(payload[:request_uri])
           if payload[:body]
             params = payload[:body]
           else
             params = Rack::Utils.parse_nested_query(uri.query)
           end
+          @grab_request.call(method, params, payload[:headers], payload)
+        end
+        handle_response(result, request_args: [method, path, *arguments])
+      rescue Timeout::Error => e
+        raise TimeoutError.new(e.message)
+      rescue OpenSSL::SSL::SSLError => e
+        raise SSLError.new(e.message)
+      end
+
+      def grab_request(&block)
+        @grab_request = block
+      end
+    end
+
+    module Methods
+      def resource_capture_controller(connection)
+        connection.grab_request do |method, params, headers, payload|
           request.headers.merge(payload[:headers])
-          yield(payload[:method], params, payload)
-          CaptureConnection.capture_id = nil
-          ActiveSupport::Notifications.unsubscribe("capture.active_resource.#{id}")
+          yield(method, params, payload)
         end
       end
 
-      def set_resource_mock_body(body)
-        body = body.to_json unless body.is_a?(String)
-        ActiveResource::TestHelper::CaptureConnection.mock_body = body
+      def resource_capture_request(connection)
+        connection.grab_request do |method, params, headers, payload|
+          yield(method, params, headers, payload)
+        end
       end
     end
   end
