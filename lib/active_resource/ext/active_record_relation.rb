0d9db344baa2b76ad1a21d5fe12558eaba935d4e
buzz <buzz@watcha.com>
2024-09-02 11:08:17 +0900

    active record relation ext

---
+++ lib/active_resource/ext/active_record_relation.rb
@@ -0,0 +1,23 @@
+ActiveSupport.on_load(:active_record) do
+  class ::ActiveRecord::PredicateBuilder::PolymorphicArrayValue
+    # Support to SomeActiveRecord.where(polymorphic_belongs_to: SomeActiveResource)
+    alias_method :__klass, :klass
+    def klass(value)
+      __klass(value) || begin
+        if value.class.respond_to?(:polymorphic_name)
+          value.class
+        end
+      end
+    end
+
+    alias_method :__convert_to_id, :convert_to_id
+    def convert_to_id(value)
+      converted = __convert_to_id(value)
+      if converted.eql?(value)
+        if value.respond_to?(:_read_attribute)
+          value._read_attribute(primary_key(value))
+        end
+      end
+    end
+  end
+end
