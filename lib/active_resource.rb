cdf43f12325ef9e9d9b885ee642dc83fd7306068
buzz <buzz@watcha.com>
2024-10-10 16:15:24 +0900

    api_type_name_object_map -> record_map

--- lib/active_resource.rb
+++ lib/active_resource.rb
@@ -46,6 +46,6 @@ module ActiveResource
 end
 
 require "active_resource/railtie" if defined?(Rails.application)
-require "active_resource/api_type_name_object_map"
+require "active_resource/record_map"
 require "active_resource/inheritance"
 require "active_resource/current"
